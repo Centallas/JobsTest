01e88cf66a3ce0ca56be21c8601f25447cd7f67e
Edward <centallas01@gmail.com>
2019-09-21 04:32:22 -0500

    Create Alter_fnGetNitData.sql
    commit

---
+++ CCG/SQLData/Function/Alter_fnGetNitData.sql
@@ -0,0 +1,21 @@
+USE [CCG]
+GO
+
+/****** Object:  UserDefinedFunction [dbo].[fnGetNitData]    Script Date: 21/09/2019 2:50:24 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+--https://stackoverflow.com/questions/44054512/how-to-use-xml-type-valued-functions-in-sql-server-for-xml
+
+ALTER FUNCTION [dbo].[fnGetNitData](@nitID int) RETURNS XML 
+AS 
+BEGIN 
+    RETURN (SELECT  [RAZONSOCIAL],[NOMBRECOMERCIO],[NIT], [CODIGOUNICO] FROM [dbo].[viewnit] WHERE [NIT] = @nitID 
+    FOR XML PATH('NIT'),TYPE)
+END
+GO
+
