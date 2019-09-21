c5a6e1e36861adb8352db5339311257c78b94aa5
Edward <centallas01@gmail.com>
2019-09-21 04:04:48 -0500

    Create Create_viewnit.sql
    fff

---
+++ CCG/SQLData/Views/Create_viewnit.sql
@@ -0,0 +1,164 @@
+USE [CCG]
+GO
+
+/****** Object:  View [dbo].[viewnit]    Script Date: 21/09/2019 3:09:34 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[viewnit]
+AS
+SELECT        dbo.MAESTRO_COMERCIOS.CODIGOUNICO, dbo.MAESTRO_COMERCIOS.NOMBRECOMERCIO, dbo.MAESTRO_COMERCIOS.RAZONSOCIAL, dbo.TERMINALES_REGISTRADAS.IDTERMINAL, 
+                         dbo.TERMINALES_REGISTRADAS.CODIGOUNICO AS tr, dbo.TERMINALES_REGISTRADAS.NIT, dbo.TERMINALES_REGISTRADAS.FECHAREGISTRO, dbo.NIT.IDNIT, dbo.NIT.DV
+FROM            dbo.MAESTRO_COMERCIOS INNER JOIN
+                         dbo.TERMINALES_REGISTRADAS ON dbo.MAESTRO_COMERCIOS.CODIGOUNICO = dbo.TERMINALES_REGISTRADAS.CODIGOUNICO INNER JOIN
+                         dbo.NIT ON dbo.TERMINALES_REGISTRADAS.IDTERMINAL = dbo.NIT.IDNIT
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_DiagramPane1', @value=N'[0E232FF0-B466-11cf-A24F-00AA00A3EFFF, 1.00]
+Begin DesignProperties = 
+   Begin PaneConfigurations = 
+      Begin PaneConfiguration = 0
+         NumPanes = 4
+         Configuration = "(H (1[41] 4[20] 2[8] 3) )"
+      End
+      Begin PaneConfiguration = 1
+         NumPanes = 3
+         Configuration = "(H (1 [50] 4 [25] 3))"
+      End
+      Begin PaneConfiguration = 2
+         NumPanes = 3
+         Configuration = "(H (1 [50] 2 [25] 3))"
+      End
+      Begin PaneConfiguration = 3
+         NumPanes = 3
+         Configuration = "(H (4 [30] 2 [40] 3))"
+      End
+      Begin PaneConfiguration = 4
+         NumPanes = 2
+         Configuration = "(H (1 [56] 3))"
+      End
+      Begin PaneConfiguration = 5
+         NumPanes = 2
+         Configuration = "(H (2 [66] 3))"
+      End
+      Begin PaneConfiguration = 6
+         NumPanes = 2
+         Configuration = "(H (4 [50] 3))"
+      End
+      Begin PaneConfiguration = 7
+         NumPanes = 1
+         Configuration = "(V (3))"
+      End
+      Begin PaneConfiguration = 8
+         NumPanes = 3
+         Configuration = "(H (1[56] 4[18] 2) )"
+      End
+      Begin PaneConfiguration = 9
+         NumPanes = 2
+         Configuration = "(H (1 [75] 4))"
+      End
+      Begin PaneConfiguration = 10
+         NumPanes = 2
+         Configuration = "(H (1[66] 2) )"
+      End
+      Begin PaneConfiguration = 11
+         NumPanes = 2
+         Configuration = "(H (4 [60] 2))"
+      End
+      Begin PaneConfiguration = 12
+         NumPanes = 1
+         Configuration = "(H (1) )"
+      End
+      Begin PaneConfiguration = 13
+         NumPanes = 1
+         Configuration = "(V (4))"
+      End
+      Begin PaneConfiguration = 14
+         NumPanes = 1
+         Configuration = "(V (2))"
+      End
+      ActivePaneConfig = 0
+   End
+   Begin DiagramPane = 
+      Begin Origin = 
+         Top = 0
+         Left = 0
+      End
+      Begin Tables = 
+         Begin Table = "MAESTRO_COMERCIOS"
+            Begin Extent = 
+               Top = 6
+               Left = 38
+               Bottom = 136
+               Right = 237
+            End
+            DisplayFlags = 280
+            TopColumn = 0
+         End
+         Begin Table = "NIT"
+            Begin Extent = 
+               Top = 78
+               Left = 660
+               Bottom = 174
+               Right = 830
+            End
+            DisplayFlags = 280
+            TopColumn = 0
+         End
+         Begin Table = "TERMINALES_REGISTRADAS"
+            Begin Extent = 
+               Top = 12
+               Left = 327
+               Bottom = 142
+               Right = 505
+            End
+            DisplayFlags = 280
+            TopColumn = 0
+         End
+      End
+   End
+   Begin SQLPane = 
+   End
+   Begin DataPane = 
+      Begin ParameterDefaults = ""
+      End
+      Begin ColumnWidths = 10
+         Width = 284
+         Width = 1500
+         Width = 1500
+         Width = 1500
+         Width = 1500
+         Width = 1500
+         Width = 1500
+         Width = 1500
+         Width = 1500
+         Width = 1500
+      End
+   End
+   Begin CriteriaPane = 
+      Begin ColumnWidths = 11
+         Column = 1440
+         Alias = 900
+         Table = 1170
+         Output = 720
+         Append = 1400
+         NewValue = 1170
+         SortType = 1350
+         SortOrder = 1410
+         GroupBy = 1350
+         Filter = 1350
+         Or = 1350
+         Or = 1350
+         Or = 1350
+      End
+   End
+End
+' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'VIEW',@level1name=N'viewnit'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_DiagramPaneCount', @value=1 , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'VIEW',@level1name=N'viewnit'
+GO
+
